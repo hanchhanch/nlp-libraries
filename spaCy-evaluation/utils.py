23937ec81e12050cc8f34c8de802ee1bed7ddd38
hanoch@gmail <hanoch.rahimi@gmail.com>
2020-05-03 14:26:50 +0300

    spaCy evaluation

---
+++ spaCy-evaluation/utils.py
@@ -0,0 +1,15 @@
+#
+#
+
+class DocParse:
+    graph = None
+    def __init__(self):
+        pass
+
+    def doc_to_graph(self, doc):
+        current_node = doc.root
+
+        #if leaf node stop
+
+    def 
+
